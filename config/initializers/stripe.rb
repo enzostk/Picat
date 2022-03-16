1f89aa3bc195ca81c94da59ac7f9701fe982f08c
Teacher-droid <teaching.know@gmail.com>
2022-03-16 15:24:38 +0100

    GitGuardian fixed

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-    :publishable_key => 'pk_live_51KdUqvHzHphRh9omC8MacCESMne1CLeB74ulp16uVXACEvkDJR3evSw9d1f1RsKjXnAktzm5UluqNv4lgxX2PoUx00HHQFEbpp',
-    :secret_key      => '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'
+    :ENV['STRIPE_PUBLIC_KEY'],
+    :ENV['STRIPE_SECRET_KEY']
   }
   
   Stripe.api_key = Rails.configuration.stripe[:secret_key]
