41ff2044b96e629bd38cdb82c42fc70e377996ed
LoisKOUNINEF <lkouninef@gmail.com>
2022-03-17 11:15:49 +0100

    add button to add items in cart and deleted controller/main and views/main

--- app/controllers/join_table_items_carts_controller.rb
+++ app/controllers/join_table_items_carts_controller.rb
@@ -1,10 +1,18 @@
 class JoinTableItemsCartsController < ApplicationController
   def create
+    @item_add = JoinTableItemsCart.add_or_create_cart_item_link(permit_link_params)
+    redirect_to items_path
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def permit_link_params
+    params.require(:join_table_items_cart).permit(:item_id, :cart_id, :quantity)
   end
 end
