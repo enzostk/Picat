0961e0cd87a8c614b8a1eb785e82e7b9ca51efa9
LoisKOUNINEF <lkouninef@gmail.com>
2022-03-17 07:36:40 +0100

    commented config/initializers/stripe.rb to be able to run rails server

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
-Rails.configuration.stripe = {
-  :ENV["STRIPE_PUBLIC_KEY"],
-  :ENV["STRIPE_SECRET_KEY"]
-}
+# Rails.configuration.stripe = {
+#   :ENV["STRIPE_PUBLIC_KEY"],
+#   :ENV["STRIPE_SECRET_KEY"]
+# }
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+# Stripe.api_key = Rails.configuration.stripe[:secret_key]
