d77387b5960ef5833f76a6646c45dde4bfc91508
Samuelkhalifa <97511680+Samuelkhalifa@users.noreply.github.com>
2022-03-17 22:32:45 +0100

    Commit branch mailer => mailer OK (en developpement)

--- config/environment.rb
+++ config/environment.rb
@@ -2,14 +2,14 @@
 require_relative 'application'
 
 # Initialize the Rails application.
-Rails.application.initialize!
+Rails.application.initialize!#
 
 
 
 ActionMailer::Base.smtp_settings = {
     :user_name => ENV['SENDGRID_LOGIN'],
     :password => ENV['SENDGRID_PWD'],
-    :domain => 'monsite.fr',
+    :domain => 'https://picat-development.herokuapp.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -62,9 +62,8 @@ Rails.application.configure do
 
 
   ### Mailer config
-  #config.action_mailer.delivery_method = :letter_opener
-   #config.action_mailer.delivery_method = :mailjet
-  config.action_mailer.perform_deliveries = true
   
+  #config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
